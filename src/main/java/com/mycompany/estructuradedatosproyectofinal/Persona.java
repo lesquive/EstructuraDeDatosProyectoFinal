5364aacc4ab3371352b2e2f6e39286828fc00524
lesquive <lesquive@LESQUIVE-RMAX5.partnet.cisco.com>
2021-03-29 20:16:32 -0600

    Persona editada

--- src/main/java/com/mycompany/estructuradedatosproyectofinal/Persona.java
+++ src/main/java/com/mycompany/estructuradedatosproyectofinal/Persona.java
@@ -9,13 +9,60 @@ package com.mycompany.estructuradedatosproyectofinal;
  *
  * @author Alegu
  */
+
 public class Persona {
-    
-    private String cedula;
-    private String nombre;
-    private String apellido;
-    private int edad;
-    private boolean colaPrioridad;
-    private String codigo;
-    
+        private String cedula;
+        private String nombre;
+        private String apellido;
+        private int edad;
+        private String codigo;
+
+     public Persona(String cedula, String nombre, String apellido, int edad, String codigo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.codigo = codigo;
+    }
+
+     public String getCedula() {
+        return cedula;
+    }
+
+     public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+     public String getNombre() {
+        return nombre;
+    }
+
+     public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+     public String getApellido() {
+        return apellido;
+    }
+
+     public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+     public int getEdad() {
+        return edad;
+    }
+
+     public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+     public String getCodigo() {
+        return codigo;
+    }
+
+     public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
 }
